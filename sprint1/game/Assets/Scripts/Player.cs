fd241d8116f7b554b6ec6ad2a907ca4dec5d2831
Caleb Price <pricecep@gmail.com>
2021-08-26 10:28:20 -0600

    Added player movement, camera, and a fire object

--- sprint1/game/Assets/Scripts/Player.cs
+++ sprint1/game/Assets/Scripts/Player.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 5.0f;
+    public float jumpSpeed = 5.0f;
     private PlayerController controller;
+    public bool isGrounded = true;
 
     // Start is called before the first frame update
     void Start()
@@ -21,5 +23,19 @@ public class Player : MonoBehaviour
         Vector3 moveDir = transform.TransformDirection(moveInput);
         Vector3 moveVec = moveDir.normalized * moveSpeed;
         controller.Move(moveVec);
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            controller.Jump(new Vector3(0.0f, jumpSpeed, 0.0f));
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionStay(Collision c)
+    {
+        if (c.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
     }
 }
--- sprint1/game/Assets/Scripts/PlayerController.cs
+++ sprint1/game/Assets/Scripts/PlayerController.cs
@@ -3,24 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(GameObject))]
 public class PlayerController : MonoBehaviour
 {
     private Vector3 velocity;
     private Rigidbody playerRigidBody;
+    private GameObject playerObject;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRigidBody = GetComponent<Rigidbody>();
+        playerObject = GameObject.Find("Temp Player");
     }
 
-    void LateUpdate()
+    void FixedUpdate()
     {
-        playerRigidBody.velocity = velocity;
+        playerRigidBody.MovePosition(transform.position + velocity * Time.deltaTime);
     }
 
     public void Move(Vector3 _velocity)
     {
         velocity = _velocity;
+    }
+
+    public void CameraRotate(float h)
+    {
+        playerObject.transform.Rotate(0.0f, h, 0.0f);
+    }
+
+    public void Jump(Vector3 jumpVec)
+    {
+        playerRigidBody.AddForce(jumpVec, ForceMode.Impulse);
     }
 }

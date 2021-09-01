09ad219530d40df898affdcc0209a45044a178f4
pricecep <pricecep@gmail.com>
2021-09-01 11:05:15 -0600

    full sprint 1 demo

--- sprint1/game/Assets/Scripts/PlayerController.cs
+++ sprint1/game/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(GameObject))]
@@ -15,6 +16,14 @@ public class PlayerController : MonoBehaviour
     {
         playerRigidBody = GetComponent<Rigidbody>();
         playerObject = GameObject.Find("Temp Player");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
     }
 
     void FixedUpdate()
